1588e10a51870b641973c459b1e16fdcf449ee72
luis <Luis.Gonzalez@southeasttech.edu>
2026-05-08 21:41:10 -0500

    Add task input

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,21 @@ function App() {
   return savedMode === "true";
 });
 
-  const [tasks] = useState(["Finish React project", "Commit changes to GitHub", "Practice presentation",])
-  
+  const [tasks, setTasks] = useState(() => {
+  const savedTasks = localStorage.getItem("tasks");
+
+  if (savedTasks) {
+    return JSON.parse(savedTasks);
+  }
+
+  return [
+    "Finish React project",
+    "Commit changes to GitHub",
+    "Practice presentation",
+  ];
+});  
+  const [newTask, setNewTask] = useState("");
+
   useEffect(() => {
   localStorage.setItem("darkMode", String(darkMode));
 }, [darkMode]);
@@ -19,6 +32,15 @@ function App() {
   useEffect(() => {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }, [tasks]);
+
+function addTask() {
+  if (newTask.trim() === "") {
+    return;
+  }
+
+  setTasks([...tasks, newTask]);
+  setNewTask("");
+}
   return (
     <main className={darkMode ? "app dark" : "app"}>
       <h1>React Task Tracker</h1>
@@ -27,10 +49,21 @@ function App() {
       <button onClick={() => setDarkMode(!darkMode)}>
         {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
       </button>
+      
+      <div>
+        <input
+        type="text"
+        placeholder="Enter a task"
+        value={newTask}
+        onChange={(event) => setNewTask(event.target.value)}
+        />
+
+        <button onClick={addTask}>Add Task</button>
+      </div>
 
       <h2> My task </h2>
 
-      <ul> {tasks.map((task, index) => (<li key={index}>{task}</li>))}</ul>
+      <ul> {tasks.map((task: string, index: number) => (<li key={index}>{task}</li>))}</ul>
 
 
     </main>
